7c2fb7689ef17f30e7822476ea5bc2255828409c
James A. Duncan <james@kinburnstreet.consulting>
2025-05-29 14:17:34 +0100

    No need to muck with the URL object, just do it all in here.

--- index.mjs
+++ index.mjs
@@ -1,22 +1,11 @@
-Object.defineProperty( window.URL.prototype, 'selector', {
-    get: function() {
-        const matchedParts = this.hash.match(/^#\(selector=(.*)\)$/);
-        if ( !matchedParts ) {
-            return null;
-        }
-        return matchedParts[1];
-    },
-    set: function( value ) {
-        this.hash = `#(selector=${value})`;
-    }
-});
-
 class SelectorRequest {
     static async fetch( urlString ) {
-        console.log("", "SelectorRequest.fetch", urlString);
         const request = new Request( urlString );
         const url = URL.parse( request.url );
-        if ( !url.selector ) {
+        
+        const matchedParts = url.hash.match(/^#\(selector=(.*)\)$/);
+        const selector = matchedParts[1];
+        if ( !selector ) {
             throw new Error("Selector not specified in the URL.");
         }
 
@@ -25,7 +14,7 @@ class SelectorRequest {
 
         if ( window.location.href === comparatorURL.toString() ) {
             // no need to go to the network if its the same thing.
-            return Array.from( document.querySelectorAll(url.selector) ).map( el => el.cloneNode( true ) );
+            return Array.from( document.querySelectorAll(selector) ).map( el => el.cloneNode( true ) );
         } else {
             const response = await fetch( request );
             if (!response.ok) {
@@ -33,7 +22,7 @@ class SelectorRequest {
             }
             const page = await response.text();
             const parsed = (new DOMParser()).parseFromString(page, 'text/html');
-            return Array.from( parsed.querySelectorAll(url.selector) );
+            return Array.from( parsed.querySelectorAll(selector) );
         }
     }
 }
