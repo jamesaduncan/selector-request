d89c5c4f068afc69bb026873e191661b1d1d4cd7
James A. Duncan <james@kinburnstreet.consulting>
2025-06-03 10:52:25 +0100

    remove spurious console.logs

--- index.mjs
+++ index.mjs
@@ -10,12 +10,10 @@ class SelectorRequest {
         return Array.from( parsed.querySelectorAll(selector) );
     }
 
-    static async fetch( urlString ) {
+    static fetch( urlString ) {
         const request = new Request( urlString );
         const url = URL.parse( request.url );
         
-        console.log(`request url is ${url.toString()}`);
-
         const matchedParts = url.hash.match(/^#\(selector=(.*)\)$/);
         const comparatorURL = URL.parse( request.url );
         comparatorURL.hash = ""; // remove the hash for comparison
@@ -27,15 +25,12 @@ class SelectorRequest {
             selector = url.hash;
         }
 
-        console.log(`comparing ${window.location.href} with ${comparatorURL.toString()}`);
         if ( window.location.href === comparatorURL.toString() ) {
             // we're local, so we do everything in this page.
-            console.log(`Fetching selector "${selector}" from local page.`);
             if (!selector) return Array.from( document.body.children ).map( el => el.cloneNode( true ) );           
             return Array.from( document.querySelectorAll(selector) ).map( el => el.cloneNode( true ) );
         } else {
             // we're remote, so we need to fetch the page.
-            console.log(`Fetching selector "${selector}" from remote page.`);
             if (!selector) return SelectorRequest.doRequest( request, "body > *" );
             return SelectorRequest.doRequest( request, selector );
 
